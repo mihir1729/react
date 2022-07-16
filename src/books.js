0faf67b4a956aa8990dc3b4236a2ecd2114abff4
Mihir Yadav <72518307+mihir1729@users.noreply.github.com>
2022-07-16 19:14:07 +0530

    react-learning

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGjytZkA-VisY8E3MKckdH-ePGtNrzS08-WQ&usqp=CAU",
+    title: "Atomic Habits",
+    author: "James Clear",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQxSRUgpwPLG-yTSVJkAZlCPm_aUGjGeNKcA&usqp=CAU",
+    title: "The Almanack Of Naval Ravikant",
+    author: "Eric Jorgenson",
+  },
+  {
+    id: 3,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVCoVPxCN_qqSvgLxDFW6N0IIuuIFjhhBSGw&usqp=CAU",
+    title: "The Psychology of Money",
+    author: "Morgan Housel",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//CSS
+import "./index.css";
+import { books } from "./books";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+  return (
+    <section className='booklist'>
+      {books.map((book) => {
+        return <Book key={book.id} {...book} />;
+      })}
+    </section>
+  );
+}
+
+const Book = ({ img, author, title }) => {
+  //attribute, eventHandler
+  //onClick, onMouseOver
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+    alert("hello world");
+  };
+  const complexHandler = (author) => {
+    console.log(author);
+  };
+  return (
+    <article
+      className='book'
+      onMouseOver={() => {
+        console.log(author);
+      }}
+    >
+      <img src={img} alt='' />
+      <h1
+        onClick={() => {
+          console.log(title);
+        }}
+      >
+        {title}
+      </h1>
+      <h4>{author}</h4>
+      <button type='button' onClick={clickHandler}>
+        reference example
+      </button>
+      <button type='button' onClick={() => complexHandler(author)}>
+        complex
+      </button>
+    </article>
+  );
+};
+
+ReactDOM.render(<BookList />, document.getElementById(`root`));
