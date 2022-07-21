dda7923cb5a964b8c094a4cf3141ce416eac4eeb
Mihir Yadav <72518307+mihir1729@users.noreply.github.com>
2022-07-22 00:00:21 +0530

    useEffect

--- src/Book.js
+++ src/Book.js
@@ -1,7 +1,40 @@
 import React from "react";
 
-const Book = () => {
-  return <div>Book</div>;
+const Book = ({ img, author, title }) => {
+  //attribute, eventHandler
+  //onClick, onMouseOver
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+    alert("hello world");
+  };
+  const complexHandler = (author) => {
+    console.log(author);
+  };
+  return (
+    <article
+      className='book'
+      onMouseOver={() => {
+        console.log(author);
+      }}
+    >
+      <img src={img} alt='' />
+      <h1
+        onClick={() => {
+          console.log(title);
+        }}
+      >
+        {title}
+      </h1>
+      <h4>{author}</h4>
+      <button type='button' onClick={clickHandler}>
+        reference example
+      </button>
+      <button type='button' onClick={() => complexHandler(author)}>
+        complex
+      </button>
+    </article>
+  );
 };
 
 export default Book;
--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import ReactDOM from "react-dom";
 //CSS
 import "./index.css";
 import { books } from "./books";
+import Book from "./Book";
+import { greeting } from "./testing/testing";
 
 function BookList() {
+  console.log(greeting);
   return (
     <section className='booklist'>
       {books.map((book) => {
@@ -14,42 +17,5 @@ function BookList() {
     </section>
   );
 }
-
-const Book = ({ img, author, title }) => {
-  //attribute, eventHandler
-  //onClick, onMouseOver
-  const clickHandler = (e) => {
-    console.log(e);
-    console.log(e.target);
-    alert("hello world");
-  };
-  const complexHandler = (author) => {
-    console.log(author);
-  };
-  return (
-    <article
-      className='book'
-      onMouseOver={() => {
-        console.log(author);
-      }}
-    >
-      <img src={img} alt='' />
-      <h1
-        onClick={() => {
-          console.log(title);
-        }}
-      >
-        {title}
-      </h1>
-      <h4>{author}</h4>
-      <button type='button' onClick={clickHandler}>
-        reference example
-      </button>
-      <button type='button' onClick={() => complexHandler(author)}>
-        complex
-      </button>
-    </article>
-  );
-};
 
 ReactDOM.render(<BookList />, document.getElementById(`root`));
